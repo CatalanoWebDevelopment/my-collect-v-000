54fa5a2eade1add43eab6e6ed80f016fb3b26f80
Parker Catalano <catalano.work@gmail.com>
2017-10-21 20:33:36 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,11 +3,11 @@ def my_collect(array)
   counter = 0
   collection = []
   while counter < array.length
-    if array == students do |student|
-      collection << student.split(" ").first
-    elsif array == languages do |language|
-    collection << language.upcase
+      if array == students do |student|
+        collection << student.split(" ").first
+      elsif array == languages do |language|
+      collection << language.upcase
+      end
     end
   collection
 end
-
